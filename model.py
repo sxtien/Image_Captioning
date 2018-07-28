57b6630e766a1b58fa2803dcd0d427b4211c06d6
Nicholas-tien <tsxcox@gmail.com>
2018-07-28 14:57:52 +0800

    RNN add init and forward

--- model.py
+++ model.py
@@ -25,18 +25,25 @@ class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
         super(DecoderRNN,self).__init__()
         self.hidden_size = hidden_size
-        self.lstm = nn.LSTM(embed_size,hidden_size,vocab_size,num_layers,batch_first=True)
+        self.lstm = nn.LSTM(embed_size,hidden_size,num_layers,vocab_size,batch_first=True)
         self.embed = nn.Embedding(vocab_size,embed_size)
         self.fc = nn.Linear(hidden_size,vocab_size)
-
     
     def forward(self, features, captions):
+        # encoderCNN output features
         features = features.view(features.size()[0],1,-1)
+        # print(features.size())
+        # training captions, take the first len-1 elements
         captions = self.embed(captions[:,:-1])
+        # print(captions.size())
+        concat_features = torch.cat((features,captions),dim=1)
+        # print(concat_features.size())
+        outputs,_ = self.lstm(concat_features)
+        outputs = self.fc(outputs)
         
+        return outputs
 
-
-        
+       
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
